f043d31131ad122b8c608cafdd34cfe9aa25e957
sameer singh <sameer8182singh@gmail.com>
2024-10-14 22:07:48 +0530

    Add project files.

---
+++ StocksApp/Program.cs
@@ -0,0 +1,16 @@
+using StocksApp;
+using StocksApp.ServiceContracts;
+using StocksApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<IFinnhubService, FinnhubService>();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
